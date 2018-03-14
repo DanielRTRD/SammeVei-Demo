a2ecc57c85b13bdd0559cfdec47173ed8e85e32b
Daniel Billing <daniel@infihex.com>
2018-03-14 20:55:11 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,33 @@
+exports.events = [
+	{
+		"id": "0",
+		"image": "Assets/map.jpg",
+		"date": "14 March",
+		"morningtime": "05:30",
+		"afternoontime": "15:00",
+		"rides": "2",
+		"rideList": [
+			{ name: "Jake", phone: 12345678 },
+            { name: "Julie", phone: 12345678 },
+        ],
+	},
+	{
+		"id": "3",
+		"image": "Assets/map.jpg",
+		"date": "14 March",
+		"morningtime": "06:00",
+		"afternoontime": "15:00",
+		"rides": "1",
+		"rideList": [
+			{ name: "Jake", phone: 12345678 },
+        ],
+	},
+	{
+		"id": "2",
+		"image": "Assets/map.jpg",
+		"date": "16 March",
+		"morningtime": "06:00",
+		"afternoontime": "15:00",
+		"rides": "0",
+	}
+];
